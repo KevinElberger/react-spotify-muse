6eb9ff5cadd096bd5f780f2154b658fd08266624
Kevin Elberger <kevelberger@gmail.com>
2022-01-26 08:02:15 -0800

    add eslint, prettier, eslint rules

--- src/components/NowPlaying/NowPlaying.test.tsx
+++ src/components/NowPlaying/NowPlaying.test.tsx
@@ -1,9 +1,9 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import { NowPlaying } from "./NowPlaying";
+import React from 'react'
+import { render } from '@testing-library/react'
+import { NowPlaying } from './NowPlaying'
 
-describe("NowPlaying", () => {
-  test("renders the NowPlaying component", () => {
-    render(<NowPlaying />);
-  });
-});
+describe('NowPlaying', () => {
+  it('renders the NowPlaying component', () => {
+    render(<NowPlaying />)
+  })
+})
--- src/components/NowPlaying/NowPlaying.tsx
+++ src/components/NowPlaying/NowPlaying.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react'
 
 export const NowPlaying = () => {
   return <div>Player component</div>
