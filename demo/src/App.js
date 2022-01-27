2d8ca93c58a84584b779c7ca18fe6445e7ec1218
Kevin Elberger <kevelberger@gmail.com>
2022-01-26 22:51:00 -0800

    add demo app

---
+++ demo/src/App.js
@@ -0,0 +1,12 @@
+import './App.css'
+import { NowPlaying } from '@kevinelberger/react-spotify-muse'
+
+function App() {
+  return (
+    <div className="App">
+      <NowPlaying />
+    </div>
+  )
+}
+
+export default App
