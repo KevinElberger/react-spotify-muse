4272756cdc16fdf696f866aba5bfae74f2bc3c34
Kevin Elberger <kevelberger@gmail.com>
2022-01-28 08:57:20 -0800

    update tsconfig excludes, re-locate spotify.ts file

--- src/components/NowPlaying/NowPlaying.tsx
+++ src/components/NowPlaying/NowPlaying.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { getNowPlaying } from '../../../lib/spotify'
+import { getNowPlaying } from '../../spotify'
 import { SpotifyApiError, SpotifyApiResponse, Track } from '../../types'
 import Logo from './logo.svg'
 
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,1 @@
-export * from './components'
+export { NowPlaying } from './components'
